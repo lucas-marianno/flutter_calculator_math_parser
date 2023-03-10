5d5745150e28b2ff09ef28a3957d1b8645867a79
Lucas Moraes <lucas.pk16@outlook.com>
2023-03-10 17:53:32 -0300

    repeated powRoot WIP

--- lib/equal_logic.dart
+++ lib/equal_logic.dart
@@ -2,22 +2,20 @@ import 'dart:math';
 
 import 'package:calculator2/buttons.dart';
 
-
-String calculateMathExpr(String s){
+String calculateMathExpr(String s) {
   // if input doesnt contain digits, returns error
-  if(!s.contains(RegExp(r'\d'))) return expressionError;
+  if (!s.contains(RegExp(r'\d'))) return expressionError;
 
   // if input contains repeated operators, returns error
-  if(s.contains(RegExp(r'[*/+-]{2,}'))) return expressionError;
+  if (s.contains(RegExp(r'[*/+-]{2,}'))) return expressionError;
 
   // gets rid of trailing .0s and returns it
-  return parentheses(s).replaceAll(RegExp(r'\.0+$'),'');
-  }
+  return parentheses(s).replaceAll(RegExp(r'\.0+$'), '');
+}
 
 const String expressionError = 'invalid expression';
 
 String parentheses(String s) {
-
   // skips this function if no parentheses are found
   if (!s.contains('(')) return addSub(multDiv(powRoot(s)));
   int prths = 0;
@@ -39,12 +37,12 @@ String parentheses(String s) {
 
   s = tempS;
 
-  // Locates the first math espression between parentheses without any 
+  // Locates the first math espression between parentheses without any
   // parentheses within it
   String betweenPar = (RegExp(r'\([^\(\)]+\)').firstMatch(s)![0]!);
-  
+
   // Removes the parentheses from both start and end, saving a clean expression
-  betweenPar= betweenPar.replaceAll(RegExp(r'[\(\)]'), '');
+  betweenPar = betweenPar.replaceAll(RegExp(r'[\(\)]'), '');
 
   s = s.replaceAll('($betweenPar)', addSub(multDiv(powRoot(betweenPar))));
   if (s.contains('(')) {
@@ -56,10 +54,16 @@ String parentheses(String s) {
 
 bool isNum(String s) => '0123456789'.contains(s);
 
-String addSub(String expression) {
+String addSub(String s) {
   //goes through a string and either adds or subtracts the next value
   //returns the sum of all operations
-  List expr = cleaner(expression);
+
+  if (s == expressionError) return expressionError;
+
+  //skips this function if there's no adition or subtraction
+  if (!s.contains('-') && !s.contains('+')) return s;
+
+  List expr = cleaner(s);
   num result = 0;
 
   for (int i = 0; i < expr.length; i++) {
@@ -74,24 +78,29 @@ String addSub(String expression) {
 
   return result.toString();
 }
+
 String powRoot(String s) {
-  if(!s.contains('\u221a') && !s.contains(ButtonId.power)) return s;
-  
   //goes through a string and executes all squareroots and power within it
   //while preserving all other symbols such as parentheses and/or sum/sub
+
+  if (s == expressionError) return expressionError;
+
+  //skips this function if there's no pow or sqrt
+  if (!s.contains('\u221a') && !s.contains(ButtonId.power)) return s;
+
   List expr = cleaner(s);
   List result = [];
 
   for (int i = 0; i < expr.length; i++) {
-    if (expr[i] == ButtonId.power || expr[i] == '\u221a') {
+    if (expr[i] == ButtonId.power || expr[i] == ButtonId.squareRoot) {
       num tempNum = expr[i + 1] == '-'
           ? num.parse(expr[i + 2]) * -1
           : num.parse(expr[i + 1]);
-      
-      if (expr[i] == '\u221a') {
-        result.last = (sqrt(num.parse(result.last))).toString();
+      if (expr[i] == ButtonId.power) {
+        result.last = pow(num.parse(expr[i - 1]), tempNum);
       } else {
-        result.last = (pow(num.parse(result.last),tempNum)).toString();
+        if (tempNum < 0) return expressionError;
+        result.add(sqrt(tempNum));
       }
       expr[i + 1] == '-' ? i += 2 : i++;
     } else {
@@ -101,10 +110,15 @@ String powRoot(String s) {
   return result.join(' ');
 }
 
-
 String multDiv(String s) {
   //goes through a string and executes all multiplications and divisions within it
   //while preserving all other symbols such as parentheses and/or sum/sub
+
+  if (s == expressionError) return expressionError;
+
+  //skips this function if there's no division or multiplication
+  if (!s.contains(ButtonId.divide) && !s.contains(ButtonId.multiply)) return s;
+
   List expr = cleaner(s);
   List result = [];
 
